a11f01a27769ef4e0965eafb6ad457824ee1e34b
ds111g <ds111g@att.com>
2021-03-09 09:51:05 -0700

    initial commit

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ def get_temp_html():
     cond_max = t.get_today_max()
     ret = f"Temp: {get_temp_str(cond[0])} <br/>" + f"Humidity: {cond[1]:.0f}% <br/>"
     ret += f"Temp Avg: {get_temp_str(cond_avg[0])} <br/>" + f"Humidity Avg: {cond_avg[1]:.0f}% <br/>"
-    ret += f"Temp Max: {get_temp_str(cond_max[0])} <br/>" + f"Temp Min: {cond_max[1]:.0f}%"
+    ret += f"Temp Max: {get_temp_str(cond_max[0])} <br/>" + f"Temp Min: {get_temp_str(cond_max[1])}"
     return ret, 200
 
 
