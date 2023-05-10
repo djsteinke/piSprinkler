0ff4344d5c2ccabaff2f61057bb1b6146ac4c293
djsteinke <djsteinke@gmail.com>
2023-05-10 14:48:20 -0600

    use firebase

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ def check_fb():
                 firebase_db.set_next_run_time(program["name"], next_date.timestamp())
                 logger.info(f"next run {next_date}")
     except Exception as e:
-        print("check() ERROR: " + str(e))
+        logger.error("check_fb(): " + str(e))
     timer = threading.Timer(60, check)
     timer.start()
 
