8fa5898d11803c3e65ed60157e583e582ebc3ca1
djsteinke <djsteinke@gmail.com>
2023-05-15 12:02:52 -0600

    use firebase

--- main.py
+++ main.py
@@ -91,7 +91,8 @@ def check_fb():
     global p_running, p
     try:
         logger.debug("check_fb()")
-        delay_date = dt.datetime.fromtimestamp(firebase_db.setup['delay'])
+        delay_timestamp = firebase_db.setup['delay'] if firebase_db.setup['delay'] > 0 else 1684254000000.0
+        delay_date = dt.datetime.fromtimestamp(delay_timestamp)
         for key in firebase_db.setup['programs']:
             logger.debug(key)
             program = firebase_db.setup['programs'][key]
